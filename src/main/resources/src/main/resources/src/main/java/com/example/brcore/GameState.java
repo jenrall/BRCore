21a72220641bd45411855fb96b13152afd0749e1
jenrall <zana.rasolipopoli@gmail.com>
2026-09-16 04:50:24 -0700

    Create GameState.java

---
+++ src/main/resources/src/main/resources/src/main/java/com/example/brcore/GameState.java
@@ -0,0 +1,9 @@
+package com.example.brcore;
+
+public enum GameState {
+    IDLE,        // بازی نیست
+    LOBBY,       // بازیکنا توی لابی
+    COUNTDOWN,   // شمارش معکوس
+    IN_GAME,     // بازی در حال اجرا
+    ENDING       // پایان بازی
+}
